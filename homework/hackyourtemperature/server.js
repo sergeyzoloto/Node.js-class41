1caf18d241d164f0a83430fdc29ceeddd1ccbb33
Sergey Zolotko <sergiozoloto@gmail.com>
2023-01-21 14:35:51 +0100

    node-fetch added

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,19 +1,25 @@
 import express from 'express';
+import { key } from './sources/keys.js';
+import fetch from 'node-fetch';
 const PORT = 3000;
 
 const app = express();
 app.use(express.json());
+//app.use('*/sources', express.static('sources'));
 
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 });
 
-app.post('/weather', (req, res) => {
-  const cityName = req.body.cityName;
-  if (!cityName) {
-    res.status(400).json({ message: 'The city is not found' });
-  } else {
-    res.json({ message: cityName });
+app.post('/weather', async function (req, res) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?APPID=${key.API_KEY}`,
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
   }
 });
 
---
+++ homework/hackyourtemperature/sources/keys.js
@@ -0,0 +1,1 @@
+export const key = { API_KEY: '9c5789e2b4574afb663653429e43f0fb' };
