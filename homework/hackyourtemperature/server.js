a8df0da184e1f5a1d20feb3be8bdd26c2ea44332
Sergey Zolotko <sergiozoloto@gmail.com>
2023-01-21 13:57:32 +0100

    json parser added

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 const PORT = 3000;
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
@@ -9,9 +10,13 @@ app.get('/', (req, res) => {
 
 app.post('/weather', (req, res) => {
   const cityName = req.body.cityName;
-  res.send(cityName);
+  if (!cityName) {
+    res.status(400).json({ message: 'The city is not found' });
+  } else {
+    res.json({ message: cityName });
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(PORT);
+  console.log('Listening to port:', PORT);
 });
