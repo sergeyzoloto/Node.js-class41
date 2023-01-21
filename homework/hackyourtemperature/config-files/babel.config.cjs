939c587443cddaa112cb01ecfa9364a69e9a1535
Sergey Zolotko <sergiozoloto@gmail.com>
2023-01-21 17:14:50 +0100

    babel config

--- homework/hackyourtemperature/config-files/babel.config.cjs
+++ homework/hackyourtemperature/config-files/babel.config.cjs
@@ -2,10 +2,10 @@ module.exports = {
   presets: [
     [
       // This is a configuration, here we are telling babel what configuration to use
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         targets: {
-          node: "current",
+          node: 'current',
         },
       },
     ],
